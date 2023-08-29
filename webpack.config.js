f527d0b20f362ee2a7801489c52fc86467bdaa15
Pavel Gorbunov <darnelo@inbox.ru>
2023-08-29 10:35:26 +0300

    copy plugin configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
@@ -16,6 +17,14 @@ module.exports = {
       template: "./index.html",
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
   ],
   resolve: {
     extensions: [".js", ".json", "..."],
